5b3d15c5478ac2fa27340674143fa0b66ca2ee76
usozer <uygarsozer2021@u.northwestern.edu>
2021-05-04 00:29:38 -0500

    First version of data upload to S3

---
+++ run.py
@@ -0,0 +1,18 @@
+import argparse
+from pathlib import Path
+
+from src.upload_data import upload
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Upload data folder to specified S3 bucket")
+    parser.add_argument("BUCKETNAME", help = "Name of S3 bucket")
+    parser.add_argument("FILENAME", help = "File name (key)")
+    
+    parser.add_argument("-d", "--DATAPATH", default="data/external/train.json", \
+                                            help = "Custom path to file", \
+                                            metavar= "") 
+
+    args = parser.parse_args()
+
+    upload(args.BUCKETNAME, args.FILENAME, args.DATAPATH)
