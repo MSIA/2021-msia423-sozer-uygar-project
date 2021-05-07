9e7e3e58867954dc970a972eee71c4896a59c226
usozer <uygarsozer2021@u.northwestern.edu>
2021-05-07 14:26:24 -0500

    Logger added, debug statements

--- run.py
+++ run.py
@@ -1,21 +1,23 @@
 import argparse
+import logging
 
 from src.upload_data import upload
 from src.data_model import create_db
 from config.dbconfig import SQLALCHEMY_DATABASE_URI, DATA_PATH
 
+logger = logging.getLogger("runner")
+
 
 if __name__ == "__main__":
-
     # Add parsers for both creating a database and adding songs to it
-    parser = argparse.ArgumentParser(description="Upload data file to S3\
-                                                  or create database")
+    parser = argparse.ArgumentParser(description="Upload data file to S3 "
+                                                 "or create database")
     subparsers = parser.add_subparsers(dest='subparser_name')
 
-    # Upload file to specified S3 bucket
+    # Subparser for S3 upload
     sp_upload = subparsers.add_parser("upload",
-                                      description="Upload data folder to\
-                                                   specified S3 bucket")
+                                      description="Upload data folder to "
+                                                  "specified S3 bucket")
     sp_upload.add_argument("bucket_name", help="Name of S3 bucket")
     sp_upload.add_argument("file_name", help="File name (key)")
     sp_upload.add_argument("-d", "--data_path",
@@ -32,9 +34,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     sp_used = args.subparser_name
+    
     if sp_used == 'create':
+        logger.debug("Create option invoked")
         create_db(args.engine_string)
     elif sp_used == 'upload':
+        logger.debug("Upload option invoked")
         upload(args.bucket_name, args.file_name, args.data_path)
     else:
         parser.print_help()
