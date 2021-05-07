3c70c3d1d365e0a528205a9fc864489d40891323
usozer <uygarsozer2021@u.northwestern.edu>
2021-05-07 14:08:23 -0500

    Exception handling, comments, logger, docstring

--- src/upload_data.py
+++ src/upload_data.py
@@ -1,7 +1,25 @@
+import logging
+
 import boto3
+
+logger = logging.getLogger(__name__)
 
 
 def upload(bucketname, filename, datapath):
+    """Upload a specified data file to an S3 bucket
+
+    Args:
+        bucketname (String): Name of S3 bucket (do not include 'S3://')
+        filename (String): Desired name of file
+        datapath (String): Location of the data file on local
+    """
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(bucketname)
-    bucket.upload_file(datapath, filename)
+
+    # Upload file to S3
+    try:
+        bucket.upload_file(datapath, filename)
+    except boto3.exceptions.S3UploadFailedError:
+        logger.error("Bucket does not exist")
+    except FileNotFoundError:
+        logger.error("File does not exist on the specified path")
