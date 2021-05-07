0a8f6ffc9a1b6db8aa9e2d9bbc00c16d4a27e611
usozer <uygarsozer2021@u.northwestern.edu>
2021-05-07 14:53:42 -0500

    Config the loggers line-by-line (removed config file input)

--- run.py
+++ run.py
@@ -5,8 +5,21 @@ from src.upload_data import upload
 from src.data_model import create_db
 from config.dbconfig import SQLALCHEMY_DATABASE_URI, DATA_PATH
 
-logger = logging.getLogger("runner")
+# Set logger configuration, prints to stdout
+logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s '
+                           '%(message)s', level=logging.DEBUG)
 
+# Only print ERROR statements from S3-related packages
+logging.getLogger("botocore").setLevel(logging.ERROR)
+logging.getLogger("s3transfer").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("boto3").setLevel(logging.ERROR)
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+logging.getLogger("aiobotocore").setLevel(logging.ERROR)
+logging.getLogger("s3fs").setLevel(logging.ERROR)
+
+# Set logger for this script
+logger = logging.getLogger("runner")
 
 if __name__ == "__main__":
     # Add parsers for both creating a database and adding songs to it
@@ -34,7 +47,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     sp_used = args.subparser_name
-    
+
     if sp_used == 'create':
         logger.debug("Create option invoked")
         create_db(args.engine_string)
