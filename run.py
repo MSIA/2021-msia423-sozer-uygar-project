309cc29cc9b6718327497e5b8ddd6eb3f074e5cf
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-02 12:14:54 -0500

    `black` formatting

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ from src.data_model import create_db
 from config.dbconfig import SQLALCHEMY_DATABASE_URI, DATA_PATH
 
 # Set logger configuration, prints to stdout
-logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s '
-                           '%(message)s', level=logging.DEBUG)
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s " "%(message)s",
+    level=logging.DEBUG,
+)
 
 # Only print ERROR statements from S3-related packages
 logging.getLogger("botocore").setLevel(logging.ERROR)
@@ -23,35 +25,42 @@ logger = logging.getLogger("runner")
 
 if __name__ == "__main__":
     # Add parsers for both creating a database and adding songs to it
-    parser = argparse.ArgumentParser(description="Upload data file to S3 "
-                                                 "or create database")
-    subparsers = parser.add_subparsers(dest='subparser_name')
+    parser = argparse.ArgumentParser(
+        description="Upload data file to S3 " "or create database"
+    )
+    subparsers = parser.add_subparsers(dest="subparser_name")
 
     # Subparser for S3 upload
-    sp_upload = subparsers.add_parser("upload",
-                                      description="Upload data folder to "
-                                                  "specified S3 bucket")
+    sp_upload = subparsers.add_parser(
+        "upload", description="Upload data folder to " "specified S3 bucket"
+    )
     sp_upload.add_argument("bucket_name", help="Name of S3 bucket")
     sp_upload.add_argument("file_name", help="File name (key)")
-    sp_upload.add_argument("-d", "--data_path",
-                           default=DATA_PATH,
-                           help="Custom path to file",
-                           metavar="")
+    sp_upload.add_argument(
+        "-d",
+        "--data_path",
+        default=DATA_PATH,
+        help="Custom path to file",
+        metavar="",
+    )
 
     # Sub-parser for creating a database
     sb_create = subparsers.add_parser("create", description="Create database")
-    sb_create.add_argument("-s", "--engine_string",
-                           default=SQLALCHEMY_DATABASE_URI,
-                           help="SQLAlchemy connection URI for database",
-                           metavar="")
+    sb_create.add_argument(
+        "-s",
+        "--engine_string",
+        default=SQLALCHEMY_DATABASE_URI,
+        help="SQLAlchemy connection URI for database",
+        metavar="",
+    )
 
     args = parser.parse_args()
     sp_used = args.subparser_name
 
-    if sp_used == 'create':
+    if sp_used == "create":
         logger.debug("Create option invoked")
         create_db(args.engine_string)
-    elif sp_used == 'upload':
+    elif sp_used == "upload":
         logger.debug("Upload option invoked")
         upload(args.bucket_name, args.file_name, args.data_path)
     else:
--- src/data_model.py
+++ src/data_model.py
@@ -13,63 +13,63 @@ Base = declarative_base()
 class Cuisine(Base):
     """Create a table for cuisines"""
 
-    __tablename__ = 'cuisines'
+    __tablename__ = "cuisines"
 
     cuisineid = Column(Integer, primary_key=True)
     name = Column(String(20), unique=False, nullable=False)
 
     # String representation
     def __repr__(self):
-        return '<Cuisine %r>' % self.title
+        return "<Cuisine %r>" % self.title
 
 
 class Ingredient(Base):
     """Create a table for ingredients"""
 
-    __tablename__ = 'ingredients'
+    __tablename__ = "ingredients"
 
     ingredientid = Column(Integer, primary_key=True)
     name = Column(String(100), unique=False, nullable=False)
 
     # String representation
     def __repr__(self):
-        return '<Ingredient %r>' % self.title
+        return "<Ingredient %r>" % self.title
 
 
 class Recipe(Base):
     """Create a table for recipes"""
 
-    __tablename__ = 'recipes'
+    __tablename__ = "recipes"
 
     recipeid = Column(Integer, primary_key=True)
-    cuisineid = Column(ForeignKey("cuisines.cuisineid"),
-                       unique=False,
-                       nullable=False)
+    cuisineid = Column(
+        ForeignKey("cuisines.cuisineid"), unique=False, nullable=False
+    )
 
     # String representation
     def __repr__(self):
-        return '<Recipe %r>' % self.title
+        return "<Recipe %r>" % self.title
 
 
 class RecipeIngredient(Base):
     """Create a linking table for ingredients"""
 
-    __tablename__ = 'recipe_ingredients'
+    __tablename__ = "recipe_ingredients"
 
     riid = Column(Integer, primary_key=True)
 
     # Create a relationship to autofill this table when
     # recipes/ingredients are added
-    recipeid = Column(ForeignKey("recipes.recipeid"),
-                      unique=False,
-                      nullable=False)
-    ingredientid = Column(ForeignKey("ingredients.ingredientid"),
-                          unique=False,
-                          nullable=False)
+    recipeid = Column(
+        ForeignKey("recipes.recipeid"), unique=False, nullable=False
+    )
+    ingredientid = Column(
+        ForeignKey("ingredients.ingredientid"), unique=False, nullable=False
+    )
 
     # String representation
     def __repr__(self):
-        return '<RecipeIngredient %r>' % self.title
+        return "<RecipeIngredient %r>" % self.title
 
 
 def create_db(engine_string):
