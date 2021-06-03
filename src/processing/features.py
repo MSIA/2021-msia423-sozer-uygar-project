df21ba243d22da08b227c93477a1c000fd7b5530
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-03 00:47:51 -0500

    feature generation

---
+++ src/processing/features.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def ingr_sum(df):
+    return df.iloc[:, 1:].sum(axis=1, skipna=True)
+
+
+def generate_train_df(df, DROP_ROWS, MIN_PREVALENCE=100):
+    cuisine_series = df.groupby("ingredient").cuisine.value_counts()
+
+    cuisinedf = cuisine_series.unstack().fillna(0)
+    cuisinedf = cuisinedf.drop(DROP_ROWS, axis=0)
+
+    cuisinedf["ingr_sum"] = ingr_sum(cuisinedf)
+
+    cuisinedf[cuisinedf.ingr_sum >= MIN_PREVALENCE]
+
+    return cuisinedf
