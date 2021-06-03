a0a402c276af3627adace41c3e35c6e88c4edcb1
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-03 10:02:55 -0500

    Transferred modeling functions inside a Class

--- src/processing/model.py
+++ src/processing/model.py
@@ -27,52 +27,66 @@ def softmax(raw):
     return np.e ** raw / np.sum(np.e ** raw)
 
 
-def predict(df, ingredients, num_guesses=3, exclude=["ingr_sum"]):
+class RecipeModel:
+    def __init__(self, num_guesses=3, num_ingredients=5):
+        self.rec_train = None
+        self.pred_train = None
 
-    try:
-        calc = df.loc[ingredients]
-    except KeyError:
-        print("key not found")
-        return
+        self.num_guesses = num_guesses
+        self.num_ingredients = num_ingredients
 
-    calc = calc.drop(exclude, axis=1).sum(axis=0)
+        self.sum_column = None
 
-    ordered = softmax(calc).sort_values(ascending=False)
+    def train(self, df, scale_const, sum_column):
+        self.sum_column = sum_column
 
-    return ordered[:num_guesses]
+        self.pred_train = df.apply(
+            normalize, exclude=[sum_column], scale=scale_const, axis=0
+        ).apply(mean_center, raw=True, axis=1)
 
-
-def recommend(df, cuisine, num_ingredients=5, selected=None):
-
-    if selected:
-        df = df.drop(labels=selected, axis=0)
-
-    ordered = df.loc[:, cuisine].sort_values(ascending=False)
-
-    return list((ordered[:num_ingredients]).index)
-
-
-def predict_and_recommend(
-    df, ingredients, scale_const=1000, num_cuisines=3, num_ingredients=5
-):
-
-    predict_train = df.apply(
-        normalize, exclude=["ingr_sum"], scale=scale_const, axis=0
-    ).apply(mean_center, raw=True, axis=1)
-    pred_cuisines = predict(
-        predict_train, ingredients, num_guesses=num_cuisines
-    )
-    pred_list = list(pred_cuisines.index)
-
-    rec_train = df.drop("ingr_sum", axis=1).apply(mean_center, raw=True, axis=0)
-    rec_list = []
-    for cuisine in pred_list:
-        recommended = recommend(
-            rec_train,
-            cuisine,
-            num_ingredients=num_ingredients,
-            selected=ingredients,
+        self.rec_train = df.drop(sum_column, axis=1).apply(
+            mean_center, raw=True, axis=0
         )
-        rec_list.append(recommended)
 
-    return dict(zip(pred_list, rec_list))
+    def predict(self, ingredients, num_guesses=3):
+
+        df = self.pred_train
+
+        try:
+            calc = df.loc[ingredients]
+        except KeyError:
+            logger.error("Some of the keys not found: %s", ingredients)
+            return
+
+        calc = calc.drop(self.sum_column, axis=1).sum(axis=0)
+
+        ordered = softmax(calc).sort_values(ascending=False)
+
+        return ordered[:num_guesses]
+
+    def recommend(self, cuisine, num_ingredients=5, selected=None):
+
+        df = self.rec_train
+
+        if selected:
+            df = df.drop(labels=selected, axis=0)
+
+        ordered = df.loc[:, cuisine].sort_values(ascending=False)
+
+        return list((ordered[:num_ingredients]).index)
+
+    def predict_and_recommend(self, ingredients):
+
+        pred_cuisines = self.predict(ingredients, num_guesses=self.num_guesses)
+        pred_list = list(pred_cuisines.index)
+
+        rec_list = []
+        for cuisine in pred_list:
+            recommended = self.recommend(
+                cuisine,
+                num_ingredients=self.num_ingredients,
+                selected=ingredients,
+            )
+            rec_list.append(recommended)
+
+        return dict(zip(pred_list, rec_list))
