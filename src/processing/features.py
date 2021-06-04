7a9437a29eab6f606321281a8c5aaad9c5cda786
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-03 22:27:44 -0500

    option to not drop any rows

--- src/processing/features.py
+++ src/processing/features.py
@@ -7,11 +7,12 @@ def ingr_sum(df):
     return df.iloc[:, 1:].sum(axis=1, skipna=True)
 
 
-def generate_train_df(df, DROP_ROWS, MIN_PREVALENCE=100):
+def generate_train_df(df, DROP_ROWS=None, MIN_PREVALENCE=100):
     cuisine_series = df.groupby("ingredient").cuisine.value_counts()
 
     cuisinedf = cuisine_series.unstack().fillna(0)
-    cuisinedf = cuisinedf.drop(DROP_ROWS, axis=0)
+    if DROP_ROWS:
+        cuisinedf = cuisinedf.drop(DROP_ROWS, axis=0)
 
     cuisinedf["ingr_sum"] = ingr_sum(cuisinedf)
 
